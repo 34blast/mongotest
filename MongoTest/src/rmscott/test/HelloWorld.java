6d78704226e3bba6b0c62e0f95a56eb8858e2c31
rmscott <rmscott@ibm-rmsmac.home>
2016-12-10 19:13:23 -0600

    initial checking

---
+++ MongoTest/src/rmscott/test/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package rmscott.test;
+
+/**
+ * @author rmscott
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] pArgs) {
+		System.out.println("Hellow World");
+	}
+
+}
---
+++ MongoTest/src/rmscott/test/TestJunk.java
@@ -0,0 +1,21 @@
+/**
+ * RMSCOTT Prototype
+ */
+package rmscott.test;
+
+/**
+ * @author rmscott
+ *
+ */
+public class TestJunk {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("TestJunk.main() : begin execution ..... ");
+		System.exit(0);;
+
+	}
+
+}
